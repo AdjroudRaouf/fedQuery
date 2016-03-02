7449f008109557346214feabe62d2ba1e41e281a
Raouf <adjroud.abderaouf@gmail.com>
2016-03-02 23:35:58 +0100

    update 2
    il reste juste les methodes utiles

--- fedQ/src/main/java/fedQ/fedQ/Methods.java
+++ fedQ/src/main/java/fedQ/fedQ/Methods.java
@@ -5,9 +5,15 @@
  */
 package fedQ.fedQ;
 
+
+import org.apache.commons.logging.LogFactory;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Vector;
+import org.antlr.runtime.ANTLRFileStream;
 import org.openrdf.OpenRDFException;
 import org.openrdf.model.Literal;
 import org.openrdf.model.URI;
@@ -15,221 +21,97 @@ import org.openrdf.model.Value;
 import org.openrdf.model.ValueFactory;
 import org.openrdf.model.vocabulary.RDF;
 import org.openrdf.query.BindingSet;
+import org.openrdf.query.BooleanQuery;
+import org.openrdf.query.GraphQueryResult;
 import org.openrdf.query.MalformedQueryException;
 import org.openrdf.query.QueryEvaluationException;
 import org.openrdf.query.QueryLanguage;
 import org.openrdf.query.TupleQuery;
 import org.openrdf.query.TupleQueryResult;
+import org.openrdf.query.resultio.sparqlxml.SPARQLResultsXMLWriter;
 import org.openrdf.repository.Repository;
 import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
 import org.openrdf.repository.sail.SailRepository;
+import org.openrdf.repository.sparql.SPARQLRepository;
 import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.turtle.TurtleWriter;
 import org.openrdf.sail.nativerdf.NativeStore;
+
 /**
  *
  * @author adjroud
  */
 public class Methods {
-    
-    public Repository CreateNativeStore(File dataDir) throws RepositoryException{
-       
 
-String indexes = "spoc,posc,cosp";
-Repository repository = new SailRepository(new NativeStore(dataDir, indexes));
-repository.initialize();
+//Creation d'un depot local
+    public Repository CreateNativeStore() throws RepositoryException {
+        File dataDir = new File("nativestore");
+        String indexes = "spoc,posc,cosp";
+        Repository repository = new SailRepository(new NativeStore(dataDir, indexes));
+        repository.initialize();
         return repository;
     }
-   
-    
-    public void AddFileRdfData(Repository repository,File file,String baseURI) throws IOException{
-        
-   
-try {
-    RepositoryConnection con = repository.getConnection();
-      con.add(file, baseURI, RDFFormat.RDFXML);
 
-}
-catch (OpenRDFException e) {
-   // handle exception
-}
-    }
-    
-   public void affichertt(Repository repository,String requete) throws RepositoryException, MalformedQueryException, QueryEvaluationException{
-      RepositoryConnection connection = repository.getConnection();  
+    //afficher le contenu d'un repo
+    public void viewAll(RepositoryConnection connection) throws RepositoryException, MalformedQueryException, QueryEvaluationException {
 
-    	TupleQuery selectQuery = connection.prepareTupleQuery(QueryLanguage.SPARQL, requete);
-    	TupleQueryResult res = selectQuery.evaluate();
+        String requete = "SELECT * WHERE {?subject ?pridicate ?object} LIMIT 100";
 
-    	while(res.hasNext()) {  
+        TupleQuery selectQuery = connection.prepareTupleQuery(QueryLanguage.SPARQL, requete);
+        TupleQueryResult res = selectQuery.evaluate();
+
+        while (res.hasNext()) {
             System.out.println("****************************************");
-    	    // chaque ligne du résultat est un BindingSet  
-    	    BindingSet aBinding = res.next();  
-
-    	    for (String aBindingName : res.getBindingNames()) {  
-    		System.out.println("La valeur de "+aBindingName+"    est     : "+aBinding.getValue(aBindingName));  
-    	 }  
-    	} 
-   }
-   
-   
-   
-   public void addStm(Repository repository,String a1,String a2,String a3){
-       ValueFactory f = repository.getValueFactory();
-URI s1 = f.createURI(a1);
-URI s2 = f.createURI(a2);
-URI s3 = f.createURI(a3);
-
-try { RepositoryConnection con = repository.getConnection();
-try {
-con.add(s1, s2, s3);
-
-
-} finally { con.close(); }
-} catch (OpenRDFException e) {}
-   }
-    
-    
-   
-   
-    public Vector GetFromRepo(Repository repository,String requete) throws RepositoryException, MalformedQueryException, QueryEvaluationException{
-      RepositoryConnection connection = repository.getConnection();  
-            
-
-Vector v = new Vector();
-System.out.println(requete);
-    	TupleQuery selectQuery = connection.prepareTupleQuery(QueryLanguage.SPARQL, requete);
-    	TupleQueryResult res = selectQuery.evaluate();
-    	// on itère sur les résultats  
-    	while(res.hasNext()) {        
-    	    // chaque ligne du résultat est un BindingSet  
-            
-    	    BindingSet aBinding = res.next();  
-
-            for (String aBindingName : res.getBindingNames()) {  
-    	
-                Value s=aBinding.getValue(aBindingName);
-                String s2=s.toString();
-                System.out.println(s2);
-               
-          
-           v.add(s2);
-      	 }
-     	} 
-        connection.close();
-        return v;
-        
-   }
-    
-    
-    
-    
-    public void AddToRepo(Vector v,Repository repo) throws RepositoryException{
-  
-   int i=0;
-        while (i<v.size()) {            
-
-    String s1=(String) v.elementAt(i);
-    String s2=(String) v.elementAt(i+1);
-    String s3=(String) v.elementAt(i+2);
-    
-   
-       addStm(repo, s1, s2, s3);
-
-i=i+3;
+            // chaque ligne du résultat est un BindingSet  
+            BindingSet aBinding = res.next();
+            //parcourer et afficher le contenu
+            for (String aBindingName : res.getBindingNames()) {
+                System.out.println("La valeur de " + aBindingName + "    est     : " + aBinding.getValue(aBindingName));
+            }
         }
     }
-    
-    
-   public Boolean askSource(String request, RepositoryConnection connection)
+//interoger les end points par ASK
+
+    public Boolean askSource(String request, RepositoryConnection connection)
             throws RepositoryException, MalformedQueryException,
             QueryEvaluationException {
 
         String requteASK = "ASK  {" + request + "}";
         System.out.println(requteASK);
-        
+
+        // on initialise la query.
         BooleanQuery booleanQuery = connection.prepareBooleanQuery(
                 QueryLanguage.SPARQL, requteASK);
 
+        // on l'exÃ©cute
         Boolean result = booleanQuery.evaluate();
 
         return result;
 
     }
-   
-   
-   
-   
-   
-   
-    public String[] getTriplePattern(String query) throws IOException {
 
-        ANTLRFileStream inputQuery = new ANTLRFileStream(query);
+    //recuperer les triples patternes
+    public String[] getTriplePattern() throws IOException {
+        ANTLRFileStream inputQuery = new ANTLRFileStream("ressources\\requete.txt");
         String req = inputQuery.toString();
         return req.split(System.getProperty("line.separator"));
 
     }
 
-    public String[] getEndPointsList(String endPoints) throws IOException {
-        // RÃ©cupÃ©rer les endPoints
-        ANTLRFileStream inputSource = new ANTLRFileStream(endPoints);
+    // Recuperer les endPoints
+    public String[] getEndPointsList() throws IOException {
+        ANTLRFileStream inputSource = new ANTLRFileStream("ressources\\endpoints.txt");
         String sources = inputSource.toString();
         String[] linesSources = sources.split(System
                 .getProperty("line.separator"));
         System.out.println(linesSources.length);
-        Vector<String> vsource = new Vector<String>();
 
-        String[] nvList = new String[vsource.size()];
-
-        for (int j = 0; j < vsource.size(); j++) {
-            nvList[j] = vsource.get(j);
-        }
         return linesSources;
     }
-    
-    
-    
-    
-    
-    
-    public Vector<Vector> getPertSources(String[] s, String[] s2) throws RepositoryException, MalformedQueryException, QueryEvaluationException {
 
-        Vector<Vector> v = new Vector<>();
-
-        for (int i = 2; i < s.length - 1; i++) {
-            for (int j = 0; j < s2.length; j++) {
-
-                String rqt = "select * where {" + s[i] + "}";
-
-                Repository rep = new SPARQLRepository(s2[j]);
-                rep.initialize();
-                RepositoryConnection c = rep.getConnection();
-
-                boolean b = askSource(rqt, c);
-                System.out.println(b);
-
-                Vector l = new Vector();
-                l.add(i);
-                l.add(j);
-                l.add(b);
-                v.add(l);
-
-            }
-        }
-
-        for (int i = 0; i < v.size(); i++) {
-            System.out.println(v.elementAt(i).elementAt(0));
-            System.out.println(v.elementAt(i).elementAt(1));
-            System.out.println(v.elementAt(i).elementAt(2));
-            System.out.println("********************************");
-        }
-
-        return v;
-    }
-    
-    
-    
-     public void FromSourceToRepo(RepositoryConnection con, String request,
+    //recuperer les resultats comme graphes et les ajouter au depot local
+    public void FromSourceToRepo(RepositoryConnection con, String request,
             RepositoryConnection connection)
             throws Exception, MalformedQueryException {
 
@@ -242,16 +124,72 @@ i=i+3;
 
         while (graphResult.hasNext()) {
 
-            graphResult.next();
-            System.out.println("graphe   :"  +graphResult.next());
-
+            //ajouter le graphe au depot local
             con.add(graphResult.next().getSubject(), graphResult.next().getPredicate(), graphResult.next().getObject());
+            //graphe suivant
+            graphResult.next();
+
+            System.out.println("graphe   :" + graphResult.next());
 
         }
 
     }
-    
-    
-    
-    
+
+//interoger les endpoints pour chaque TP
+    public HashMap<MapKey, Boolean> askSourceForTriplePattern(
+            String[] triplePattern) throws IOException, RepositoryException,
+            MalformedQueryException, QueryEvaluationException {
+
+        String[] linesSources = getEndPointsList();
+        HashMap<MapKey, Boolean> askResult = new HashMap<MapKey, Boolean>();
+        RepositoryConnection con;
+        // Parcourir les endPoints pour faire une requete Ask
+        for (int i = 0; i < linesSources.length; i++) {
+            // Demande de connection à la source
+
+            Repository r = new SPARQLRepository(linesSources[i]);
+            r.initialize();
+            con = r.getConnection();
+            for (int j = 0; j < triplePattern.length; j++) {
+                // Récuperer le résultat
+                askResult.put(new MapKey(i, j), askSource(triplePattern[j], con));
+            }
+            con.close();
+        }
+        return askResult;
+    }
+
+    // recuperer les resultats et les ajouter dans le depot local
+    public void getResults(RepositoryConnection con,
+            HashMap<MapKey, Boolean> askResult)
+            throws Exception {
+        //recuperer les triples patterns et les endpoints
+        String[] linesSources = getEndPointsList();
+        String[] triplePattern = getTriplePattern();
+
+        RepositoryConnection connection;
+
+        // Parcourir les endPoints pour faire un Select
+        for (int i = 0; i < linesSources.length; i++) {
+            // Demande de connection Ã  la source
+            Repository r = new SPARQLRepository(linesSources[i]);
+            r.initialize();
+            connection = r.getConnection();
+
+            for (int j = 0; j < triplePattern.length; j++) {
+                // Si le Ask est true, on exucute pour récupérer le résultat
+                if (askResult.get(new MapKey(i, j)) != null
+                        && askResult.get(new MapKey(i, j))) {
+					// nombreDeResultat = nombreDeResultat +
+
+                    // charger les fichier dans un repo && Interroger le depot local
+                    FromSourceToRepo(con, triplePattern[j], connection);
+
+                }
+            }
+
+            connection.close();
+        }
+
+    }
 }
